3b327018d71d68e63e6a5f13163066bcf0fa74a6
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-01-22 13:09:20 +0530

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = '--or%g#*34#8xaq2ool&489i1ws)z3@h$2j+wx23&o%kk!1-)g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','xeroxap.herokuapp.com']
 
 
 # Application definition

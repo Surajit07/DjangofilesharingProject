89498dad854bd50178ec537c930c7379593a7d44
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-01-22 13:57:12 +0530

    Update views.py

--- photos/views.py
+++ photos/views.py
@@ -9,18 +9,6 @@ def index(request):
         )
         new_photo.save()
         new_url=str('https://xeroxfin.herokuapp.com'+new_photo.file.url)
-        from twilio.rest import Client
-        sid='ACad20495dd3bf324541f3c9a60657ddf9'
-        authToken='79dd15fb319bc03f63c7d81f4f8a662c'
-
-        client=Client(sid,authToken)
-
-        from_whatsapp_number='whatsapp:+14155238886'
-        to_whatsapp_number='whatsapp:+919863103113'
-
-        message=client.messages.create(body=new_url,
-                                   from_=from_whatsapp_number,
-                                   to=to_whatsapp_number)
         return render(request, 'index.html', {'new_url': new_url})
     else:
         return render(request, 'index.html')

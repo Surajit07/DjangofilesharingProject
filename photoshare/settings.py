c38880eba5738a7a65cbbdceff40e710281bc037
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-01-22 13:46:14 +0530

    Update settings.py

--- photoshare/settings.py
+++ photoshare/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '--or%g#*34#8xaq2ool&489i1ws)z3@h$2j+wx23&o%kk!1-)g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','xeroxfin.herokuapp.com']
 

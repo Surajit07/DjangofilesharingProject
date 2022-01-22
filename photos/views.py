387a5d95496474797296e9bc99b890bcbd964850
Arghajit08 <89945847+Arghajit08@users.noreply.github.com>
2022-01-22 13:03:03 +0530

    Add files via upload

---
+++ photos/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from .models import Photo
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        new_photo = Photo(
+            file = request.FILES['img']
+        )
+        new_photo.save()
+        new_url=str(new_photo.file.url)
+        from twilio.rest import Client
+        sid='ACad20495dd3bf324541f3c9a60657ddf9'
+        authToken='79dd15fb319bc03f63c7d81f4f8a662c'
+
+        client=Client(sid,authToken)
+
+        from_whatsapp_number='whatsapp:+14155238886'
+        to_whatsapp_number='whatsapp:+919863103113'
+
+        message=client.messages.create(body=new_url,
+                                   from_=from_whatsapp_number,
+                                   to=to_whatsapp_number)
+        return render(request, 'index.html', {'new_url': str(new_photo.file.url)})
+    else:
+        return render(request, 'index.html')
